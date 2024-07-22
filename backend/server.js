b14dd295d622f6d420e91e6fb472371db0c2fd3a
adeyemi <mathewadeyemi7654@gmail.com>
2024-07-22 18:28:47 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const port = process.env.port || 4000;
 connectDB();
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.get("/", (req, res) => res.send("welcome to blog app"));
 app.use((err, req, res, next) => {
   res.status(500).send({ message: `My Mistake= ${err.message}` });
 });
